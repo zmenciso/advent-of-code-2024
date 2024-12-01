c385444b914d3e83aca613f8d7b60d615750d551
Zephan Enciso <zmenciso@pm.me>
2024-12-01 16:19:40 -0500

    Part 1

---
+++ aoc-01/src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::iter::zip;
+
+fn main() {
+    let mut v1: Vec<i64> = Vec::new();
+    let mut v2: Vec<i64> = Vec::new();
+
+    // Ingest input
+    for line in io::stdin().lines() {
+        let line = line.expect("Failed to read line");
+        let mut iter = line.split_whitespace();
+
+        let n1 = iter.next()
+            .expect("Missing number")
+            .parse::<i64>()
+            .expect("Could not parse number");
+
+        let n2 = iter.next()
+            .expect("Missing number")
+            .parse::<i64>()
+            .expect("Could not parse number");
+
+        v1.push(n1);
+        v2.push(n2);
+    }
+
+    // Sort vectors
+    v1.sort();
+    v2.sort();
+
+    // Find the distances between the two lists
+    let diff: Vec<i64> = zip(v1, v2)
+        .map(|(a, b)| (b - a).abs())
+        .collect();
+
+    // Find the total distance
+    let sum: i64 = diff.iter().sum();
+
+    println!("{}", sum);
+}
