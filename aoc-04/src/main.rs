bbd1751f92824917e2f7a66d548e2c0090a0ee56
Zephan Enciso <zmenciso@pm.me>
2024-12-04 10:32:50 -0500

    Day 4: Part A

---
+++ aoc-04/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+use aoc_04::Grid;
+
+fn main() {
+    let mut g = Grid::new();
+
+    for line in io::stdin().lines() {
+        let line = line.expect("Failed to read line");
+        g.insert_row(&line);
+    }
+
+    println!("{}", g.search_all("XMAS".as_bytes()));
+}
