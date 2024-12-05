cb81f2521abb48c30386bc2d2b98124399017e46
Zephan Enciso <zmenciso@pm.me>
2024-12-05 13:21:45 -0500

    Day 5: Complete

--- aoc-05/src/lib.rs
+++ aoc-05/src/lib.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
+type Rule = Vec<usize>;
+
 #[derive (Debug)]
 pub struct Rules {
-    r: HashMap<usize, Vec<usize>>,
+    r: HashMap<usize, Rule>,
 }
 
 impl Rules {
@@ -33,7 +35,7 @@ impl Rules {
     }
 
     /// Returns a list of all numbers that must come before num
-    pub fn retrieve(&self, num: usize) -> Option<&Vec<usize>> {
+    pub fn retrieve(&self, num: usize) -> Option<&Rule> {
         self.r.get(&num)
     }
 }
@@ -55,20 +57,48 @@ impl Update {
         Update {p, mid}
     }
 
-    pub fn check_valid(&self, rules: &Rules) -> bool {
+    pub fn find_fault(&self, rules: &Rules) -> Option<usize> {
         for i in 0 .. self.p.len() {
             let num = self.p[i];
 
             if let Some(before) = rules.retrieve(num) {
                 // Check to make sure none of before comes after
                 if before.into_iter().
-                    any(|x| self.p[i..].contains(x)) { return false }
+                    any(|x| self.p[i..].contains(x)) { return Some(i) }
             }
             else {
                 continue
             }
 
         }
+        None
+    }
+
+    /// Returns true if the update was invalid
+    pub fn fix_invalid(&mut self, rules: &Rules) -> bool {
+        if let Some(loc) = Self::find_fault(self, rules) {
+            let num = self.p[loc];
+            // Get violating Rule
+            // TODO: Have find_fault also return the Rule
+            let before = rules.retrieve(num).unwrap();
+
+            // Scan right and place the number after all items in before
+            assert_eq!(self.p.remove(loc), num);
+            let mut i = loc;
+            while before.into_iter().any(|x| self.p[i..].contains(x)) {
+                i += 1;
+            }
+            self.p.insert(i, num);
+
+            // Do it again
+            Self::fix_invalid(self, rules);
+        }
+        else {
+            return false
+        }
+
+        // Update midpoint
+        self.mid = self.p[self.p.len() / 2];
         true
     }
 }
