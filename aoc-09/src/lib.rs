131ab1be566d54992a312fb89e734e01fd5f9867
Zephan Enciso <zmenciso@pm.me>
2024-12-09 10:58:02 -0500

    I allocated the whole number, I'mma use the whole number

--- aoc-09/src/lib.rs
+++ aoc-09/src/lib.rs
@@ -3,7 +3,7 @@ use std::fmt;
 
 #[derive (Debug)]
 pub struct Disk {
-    d: Vec<i16>
+    d: Vec<u16>
 }
 
 impl fmt::Display for Disk {
@@ -30,12 +30,12 @@ impl Disk {
             .map(|x| x as u8)
             .tuples();
 
-        let mut disk: Vec<i16> = Vec::new();
+        let mut disk: Vec<u16> = Vec::new();
 
         // HERP DERP
         for (id, (blocks, free)) in dense.enumerate() {
-            let file = vec![id as i16; blocks as usize];
-            let space = vec![-1_i16; free as usize];
+            let file = vec![id as u16; blocks as usize];
+            let space = vec![u16::MAX; free as usize];
 
             disk.extend(file);
             disk.extend(space);
@@ -48,20 +48,20 @@ impl Disk {
 
     /// Returns false when no blocks moved (fully defragged)
     fn move_block(&mut self) -> bool {
-        let i = self.d.iter().rposition(|&x| x != -1)
+        let i = self.d.iter().rposition(|&x| x != u16::MAX)
             .expect("No data");
 
-        if !self.d[0..i].contains(&-1) {
+        if !self.d[0..i].contains(&u16::MAX) {
             // Disk fully defragged
             return false
         }
         
-        let free = self.d.iter().position(|&x| x == -1)
+        let free = self.d.iter().position(|&x| x == u16::MAX)
             .expect("No free space");
 
         let block = self.d.remove(i);
         self.d[free] = block;
-        self.d.push(-1_i16);
+        self.d.push(u16::MAX);
 
         true
     }
@@ -75,7 +75,7 @@ impl Disk {
     pub fn checksum(&self) -> u64 {
         let mut sum: u64 = 0;
 
-        for (i, val) in self.d.iter().filter(|&x| *x >= 0).enumerate() {
+        for (i, val) in self.d.iter().filter(|&x| *x < u16::MAX).enumerate() {
             sum += i as u64 * *val as u64;
         }
 
