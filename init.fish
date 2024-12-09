0f7da030c009a61bb02ae3a7b53514ba7d18923a
Zephan Enciso <zmenciso@pm.me>
2024-12-09 17:36:29 -0500

    Mixed tabs/spaces would hurt me

--- init.fish
+++ init.fish
@@ -3,9 +3,9 @@
 set template "use std::io;
 
 fn main() {
-	for line in io::stdin().lines() {
-		let line = line.expect(\"Could not read line\");
-	}
+    for line in io::stdin().lines() {
+        let line = line.expect(\"Could not read line\");
+    }
 }"
 
 set dir "aoc-$argv[1]"
