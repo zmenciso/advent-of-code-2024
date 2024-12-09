f247716477f6156e14171b5bf797ba59d3c81bbf
Zephan Enciso <zmenciso@pm.me>
2024-12-09 17:35:30 -0500

    I'm even too lazy to type `use std::io`

--- init.fish
+++ init.fish
@@ -1,6 +1,16 @@
 #!/usr/bin/fish
 
+set template "use std::io;
+
+fn main() {
+	for line in io::stdin().lines() {
+		let line = line.expect(\"Could not read line\");
+	}
+}"
+
 set dir "aoc-$argv[1]"
 mkdir $dir
 cd $dir
+
 cargo init
+echo $template > src/main.rs
